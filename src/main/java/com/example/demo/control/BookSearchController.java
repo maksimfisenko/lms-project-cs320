3554691d2c5ae62fa9aa045dfe20dd2ade926752
Gleb Shkoda <gleb.shkoda@gmail.com>
2024-01-02 18:37:45 +0300

    Search Controller

--- src/main/java/com/example/demo/control/BookSearchController.java
+++ src/main/java/com/example/demo/control/BookSearchController.java
@@ -1,12 +1,20 @@
 package com.example.demo.control;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class BookSearchController extends Controller {
 
+    @FXML
+    TextField searchField;
     public void Back (ActionEvent e) throws IOException {
-        this.LoadScene("AdminWelcome.fxml", e);
+        this.LoadScene("UserWelcome.fxml", e);
+    }
+
+    public void StartSearchBar(String search){
+        searchField.setText(search);
     }
 }
