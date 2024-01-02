2eb44d62a9ae56ed024d917d37d0d69979c9f01a
Gleb Shkoda <gleb.shkoda@gmail.com>
2024-01-02 20:27:59 +0300

    Add New Book Work

--- src/main/java/com/example/demo/control/AdminAddBookController.java
+++ src/main/java/com/example/demo/control/AdminAddBookController.java
@@ -1,21 +1,49 @@
 package com.example.demo.control;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class AdminAddBookController extends Controller {
 
+    ObservableList<String> genreList = FXCollections.observableArrayList(
+            "Classic", "Adventure", "Science", "Detective", "Study"
+    );
+    ObservableList<String> coverList = FXCollections.observableArrayList(
+            "Hardcover", "Paperback"
+    );
+    ObservableList<String> conditionList = FXCollections.observableArrayList(
+            "Very Good", "Good", "Acceptable", "Not Acceptable"
+    );
+
     @FXML
     private TextField titleField;
+    @FXML
+    private ComboBox<String> genreBox;
+    @FXML
+    private ComboBox<String> coverBox;
+    @FXML
+    private ComboBox<String> conditionBox;
+
     public void Back (ActionEvent e) throws IOException {
         this.LoadScene("AdminWelcome.fxml", e);
     }
 
     public void Save (ActionEvent e) throws IOException {
 
+    }
+
+    @FXML
+    public void initialize(){
+        genreBox.setItems(genreList);
+        conditionBox.setItems(conditionList);
+        coverBox.setItems(coverList);
     }
 
 }
