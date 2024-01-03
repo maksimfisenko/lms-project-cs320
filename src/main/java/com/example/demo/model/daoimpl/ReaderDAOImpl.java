29b4f42a75a8a948bfd855d9f161497f1715512d
Mohammed-Mqbel <mohammedali.moqbel@ozu.edu.tr>
2024-01-03 04:46:16 +0300

    commit

--- src/main/java/com/example/demo/model/daoimpl/ReaderDAOImpl.java
+++ src/main/java/com/example/demo/model/daoimpl/ReaderDAOImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.model.daoimpl;
 
 import com.example.demo.model.dao.ReaderDAO;
+import com.example.demo.model.entities.Book;
 import com.example.demo.model.entities.Reader;
 
 import java.sql.*;
@@ -16,11 +17,23 @@ public class ReaderDAOImpl implements ReaderDAO {
 
     @Override
     public void addReader(Reader reader) {
-        String insertQuery = "INSERT INTO readers (name, email) VALUES (?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-            preparedStatement.setString(1, reader.getName());
-            preparedStatement.setString(2, reader.getLogin());
+        String query = "INSERT INTO readers (login, password, name, surname, num_of_strikes) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, reader.getLogin());
+            preparedStatement.setString(2, reader.getPassword());
+            preparedStatement.setString(3,reader.getName());
+            preparedStatement.setString(4,reader.getSurname());
+            preparedStatement.setInt(5,reader.getNumOfStrikes());
+
             preparedStatement.executeUpdate();
+
+            Statement statement = connection.createStatement();
+            ResultSet generatedKeys = statement.executeQuery("SELECT last_insert_rowid()");
+            if (generatedKeys.next()) {
+                int generatedKey = generatedKeys.getInt(1);
+                reader.setId(generatedKey);
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -28,13 +41,15 @@ public class ReaderDAOImpl implements ReaderDAO {
 
     @Override
     public Reader getReaderById(int id) {
-        String selectQuery = "SELECT * FROM readers WHERE id = ?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
+        String query = "SELECT * FROM readers WHERE id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, id);
+
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
-                    return new Reader(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("login"));
-                }
+                    return mapResultSetToReader(resultSet);
+                     }
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -45,10 +60,13 @@ public class ReaderDAOImpl implements ReaderDAO {
     @Override
     public List<Reader> getAllReaders() {
         List<Reader> readers = new ArrayList<>();
-        String selectAllQuery = "SELECT * FROM readers";
-        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(selectAllQuery)) {
+        String query = "SELECT * FROM readers";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
             while (resultSet.next()) {
-                readers.add(new Reader(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("login")));
+                readers.add(mapResultSetToReader(resultSet));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -58,11 +76,15 @@ public class ReaderDAOImpl implements ReaderDAO {
 
     @Override
     public void updateReader(Reader reader) {
-        String updateQuery = "UPDATE readers SET name = ?, email = ? WHERE id = ?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
-            preparedStatement.setString(1, reader.getName());
-            preparedStatement.setString(2, reader.getLogin());
-            preparedStatement.setInt(3, reader.getId());
+        String query = "UPDATE readers SET login = ?, password = ?, name = ?, surname = ?, num_of_strikes = ?  WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, reader.getLogin());
+            preparedStatement.setString(2, reader.getPassword());
+            preparedStatement.setString(3, reader.getName());
+            preparedStatement.setString(4, reader.getSurname());
+            preparedStatement.setInt(5,reader.getNumOfStrikes());
+            preparedStatement.setInt(6, reader.getId());
+
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -71,12 +93,28 @@ public class ReaderDAOImpl implements ReaderDAO {
 
     @Override
     public void deleteReader(int id) {
-        String deleteQuery = "DELETE FROM readers WHERE id = ?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery)) {
+        String query = "DELETE FROM readers WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setInt(1, id);
             preparedStatement.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private Reader mapResultSetToReader(ResultSet resultSet) throws SQLException {
+
+        Reader reader = new Reader(
+                resultSet.getInt("id"),
+                resultSet.getString("login"),
+                resultSet.getString("password"),
+                resultSet.getString("name"),
+                resultSet.getString("surname")
+        );
+        reader.setNumOfStrikes(resultSet.getInt("num_of_strikes"));
+
+        return reader;
     }
 }
